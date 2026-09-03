af010bf624bcef02b40b06fcfd33e91ad04b062f
Edwin <grusgrus829@gmail.com>
2026-09-03 17:39:46 +0200

    klar för redovisning

--- gameloop.js
+++ gameloop.js
@@ -9,11 +9,7 @@ var deck = [];
 var suits = ["diamonds", "spades", "hearts", "clubs"];
 var values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 
-suits.forEach(function(suits) {
-    values.forEach(function(value) {
-    deck.push(`${value} of ${suits}`);
-  });
-});
+
 function newRound() {
     player = new Player([]);
     dealer = new Dealer([]);
@@ -134,6 +130,7 @@ function checkWin() {
         document.getElementById("money").innerText = `Pengar: ${money} kr`;
         document.getElementById("stand").disabled = true;
         document.getElementById("reset").style.backgroundColor = "yellow";  
+        document.getElementById("reset").style.color = "black"
     }
 
         
@@ -193,13 +190,14 @@ startGameButton.addEventListener("click", () => {
     mainGame.classList.remove("hidden");
     activeBetElement.innerText = `Nuvarande insats: ${currentBet} kr`;
     reset.style.backgroundColor = "";
+    reset.style.color = "";
     newRound();
 });
 
 
 function placeBet(amount) {
     if (currentBet + amount > money) {
-        document.getElementById("betERROR").innerText = "BITCH! Du kan inte satsa mer pengar än vad du har!";
+        document.getElementById("betERROR").innerText = "Du kan inte satsa mer pengar än vad du har!";
         return;
     }
     currentBet += amount;
